11e8ce9f3ac0c0ce8ee333ef412858284a6bbe1a
Alexis Castillo <uclastle9112342@gmail.com>
2025-12-01 12:10:35 -0600

    feat:practica de switch

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -76,10 +76,122 @@ const weatherCode = (code) =>{
 console.log(weatherCode(45));
 let img = document.getElementById("imagen");
 
-const estadoAnimo = emocion  =>{
-    if(emocion===true){
+const estadoAnimo = emocion =>{
+    if(emocion===false){
     img.src = "public/images/dino.jpg";
     }else{
-         img.src = "public/images/dino-emo.png";
+         img.src = "./public/images/dino.jpg";
     }
 }
+
+//--------------- Condicional Switch-case --------------------------
+/*
+ La condicional Switch evalua una expresión y se compara
+ con el valor de cada instancia en 'case' y se ejecuta las
+ sentencias asociadas a ese 'case' hasta encontrar
+ la sentencia 'break' o se finalicen todas las sentencias
+ de la condicional switch.
+
+ Para comparar la condicional switch usa el operador estricto ===
+
+ Sintaxis:
+  switch (expresión) {
+    case valor1:
+        sentencia;
+        break;
+    case valor 2:
+        sentencias;
+        break;
+    case valor n:
+        sentencias;
+        break
+    default:
+        sentencias;
+        break;
+  }
+
+*/
+
+
+const getAccessLevel = ( role ) => {
+    let accessLevel;
+    switch ( role ) {
+        case "admin":
+            accessLevel = "Acceso completo al sistema";
+            break;
+        case "editor":
+            accessLevel = "Acceso para editar contenido";
+            break;  
+        case "viewer":
+            accessLevel = "Acceso solo para ver contenido";
+            break;
+        default:
+            accessLevel = "Acceso denegado";
+    }
+    return accessLevel;
+};
+console.log( getAccessLevel("admin") ); // Acceso completo al sistema
+console.log( getAccessLevel("super_admin") ); // Acceso completo al sistema
+console.log( getAccessLevel("editor") ); // Acceso para editar contenido
+console.log( getAccessLevel("customer") ); // Acceso denegado
+ /*
+ Refactorizar la función getWeather usando switch-case
+ const getWeather=(codigo)=>{
+     let message;
+    if(codigo===0){
+        message="Clear Sky";
+    }else if( codigo===1 ||codigo===2 ||codigo===3  ){
+        message="Mainly clear, partly cloudy, and overcast";
+    }else if(codigo===45 || codigo===48){
+        message="Fog and depositing rime fog";
+    }else{
+        message="no definido";
+    }
+    return message;
+   
+}
+*/
+const getWeatherSwitch=(codigo)=>{ 
+    let message;
+    switch(codigo){
+        case 0:
+       message = "Clear Sky";
+       break
+       case 1:
+       case 2:
+       case 3:
+      message="Mainly clear, partly cloudy, and overcast";
+      break;
+      case 45:
+      case 48:
+      message="Fog and depositing rime fog";
+      break;
+      default:
+      message="no definido";
+    }
+     return message;
+}
+console.log(getWeatherSwitch(0));
+// ------------------------ Operador ternario --------------------------
+/*
+ Es el único operador de JavaScript que tiene 3 operandos.
+ Generalmente se utiliza como opción a la sentencia if-else.
+ Sintaxis:
+  condición ? expresiónSiCondiciónEsVerdadera : expresionSiCondiciónEsFalsa; 
+*/
+
+const votingEligibility = (age) =>{
+    return age =>18 ? "Puede votar": "No puede votar";
+}
+console.log(votingEligibility(20));
+console.log(votingEligibility(16));
+
+/*
+ Realizar una función que reciba un número y retorne
+ "Par" si el número es par o "Impar" si el número es impar.
+ Usar el operador ternario.
+*/
+const  sabersiEspar = (numero)=>{
+    return numero%2===0 ? "El numero es par": "El numero es impar";
+}
+console.log(sabersiEspar(3));
